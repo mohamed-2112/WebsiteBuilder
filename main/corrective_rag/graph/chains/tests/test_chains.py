dae534fc7ce36950d5bdd19842f6c3716b980deb
mohamed-2112 <mohamed.rasmy.fathy@gmail.com>
2024-07-14 23:34:52 -0400

    implementing the nodes

--- main/corrective_rag/graph/chains/tests/test_chains.py
+++ main/corrective_rag/graph/chains/tests/test_chains.py
@@ -1,2 +1,24 @@
-def test_foo() -> None:
-    assert 1==1
+from dotenv import load_dotenv
+
+load_dotenv("D:\PycharmProjects\WebsiteBuilder\config\.env")
+
+from main.corrective_rag.graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from main.corrective_rag.ingestion import retriever
+
+
+def test_retrieval_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke({"question": question, "document": doc_txt})
+    assert res.binary_score == "yes"
+
+
+def test_retrieval_grader_answer_no() -> None:
+    question = "how to make pizaa"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke({"question": question, "document": doc_txt})
+    assert res.binary_score == "no"
