1e08d569d13c47ac522dc2463017d877d0440a78
mohamed-2112 <mohamed.rasmy.fathy@gmail.com>
2024-07-21 23:38:18 -0400

    creating a grader

--- main/corrective_rag/graph/chains/tests/test_chains.py
+++ main/corrective_rag/graph/chains/tests/test_chains.py
@@ -6,6 +6,7 @@ from main.corrective_rag.graph.chains.retrieval_grader import GradeDocuments, re
 from main.corrective_rag.ingestion import retriever
 from pprint import pprint
 from main.corrective_rag.graph.chains.generation import generation_chain
+from main.corrective_rag.graph.chains.hallucination_grader import hallucination_grader, GradeHallucination
 
 
 def test_retrieval_grader_answer_yes() -> None:
@@ -25,8 +26,24 @@ def test_retrieval_grader_answer_no() -> None:
     res: GradeDocuments = retrieval_grader.invoke({"question": question, "document": doc_txt})
     assert res.binary_score == "no"
 
+
 def test_generation_chain() -> None:
     question = "agent memory"
     docs = retriever.invoke(question)
     generation = generation_chain.invoke({"context": docs, "question": question})
     pprint(generation)
+
+
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    generation = generation_chain.invoke({"context": docs, "question": question})
+    res: GradeHallucination = hallucination_grader.invoke({"documents": docs, "generation": generation})
+    assert res.binary_score
+
+
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    res: GradeHallucination = hallucination_grader.invoke({"documents": docs, "generation": "in order to make pizza you start with the dough"})
+    assert not res.binary_score
